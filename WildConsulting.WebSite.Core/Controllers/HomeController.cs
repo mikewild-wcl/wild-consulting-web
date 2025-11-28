ba09c292371bfedfcb33f114638962f15f639507
Mike Wild <wildconsultingltd@googlemail.com>
2025-11-28 14:37:50 +0000

    Initial Claude rewrite

--- WildConsulting.WebSite.Core/Controllers/HomeController.cs
+++ WildConsulting.WebSite.Core/Controllers/HomeController.cs
@@ -8,16 +8,6 @@ public class HomeController(IConfiguration configuration) : Controller
 {
     public IActionResult Index()
     {
-        return View();
-    }
-
-    public IActionResult About()
-    {
-        return View();
-    }
-
-    public IActionResult Contact()
-    {
         var emailAddress = configuration["ContactSettings:Email"];
         var vm = new ContactViewModel(emailAddress);
         return View(vm);
